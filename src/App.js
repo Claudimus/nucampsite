30760b53160ebec7b208de434487e3c35fd63355
Claudio8 <jccantave@gmail.com>
2023-11-18 13:45:05 -0500

    My changes after 11/18/23 workshop.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-
-import './App.css';
+import React from "react";
+import { Container, Navbar, NavbarBrand } from "reactstrap";
+import NucampLogo from "./app/assets/img/logo.png";
+import "./App.css";
+import CampsiteCard from "./features/campsites/CampsiteCard.js";
+import { CAMPSITES } from "./app/shared/CAMPSITES";
 
 function App() {
   return (
     <div className="App">
-      hello nucamp !
+      <Navbar dark color="primary" sticky="top" expand="md">
+        <Container>
+          <NavbarBrand href="/">
+            <img src={NucampLogo} alt="nucamp logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+
+      <CampsiteCard campsite={CAMPSITES[0]} />
     </div>
   );
 }
